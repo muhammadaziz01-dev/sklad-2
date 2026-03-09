59bf9c1a5e2e4280070e5db77175a545bf19343e
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2026-03-09 23:45:29 +0500

    deshbord abdetet

--- src/views/deshbord/index.vue
+++ src/views/deshbord/index.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="dashboard">
-    <h2>Dashboard</h2>
-    <nav>
-      <router-link to="/dashboard">Home</router-link> |
-      <router-link to="/dashboard/settings">Settings</router-link>
+    <div class="dashboard--info-card">
+      <div class="dashboard--info-card--title">
+        <p>Омбор қиймати</p>
+        <img :src="Sclad" alt="Sclad icon" class="dashboard--info-card--title--img"  >
+      </div>
+      <div class="dashboard--info-card--statis">
+        <div class="dashboard--info-card--statis--title">
+          <h4><count-up :end-val="9545345"></count-up> <span>сом</span> </h4>
+          <p><span><count-up :end-val="34"></count-up></span> хил маҳсулот </p>
+        </div>
+        <img :src="Diogram" alt="statistika icon">
+      </div>
+    </div>
+    <nav class="dashboard--navbar" >
+      <router-link to="/dashboard">
+        <img :src="RTP" alt="RTP logo">
+      </router-link> 
+      <router-link to="/dashboard/settings">
+      <img :src="VALFEX" alt="Valfex logo">
+      </router-link>
     </nav>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'DashboardPage'
-};
+<script setup>
+import RTP from "../../assets/rtp-logo.png";
+import VALFEX from "../../assets/valfex-logo.png";
+import Sclad from "../../assets/sclad.png";
+import Diogram from "../../assets/diogram.svg"
+import CountUp from 'vue-countup-v3'
+
+
 </script>
+
+<style scoped lang="scss" src="./style.scss"></style>
