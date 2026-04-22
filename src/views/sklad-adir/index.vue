68ed9b1c8bec3225f8df3e9e828da5b19b795a43
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2026-04-22 10:00:27 +0500

    skald adir page cretet

--- src/views/sklad-adir/index.vue
+++ src/views/sklad-adir/index.vue
@@ -1,12 +1,55 @@
 <template>
     <Navbar :props="propsDate" />
-    <div>
-     <h1>Склад адир</h1>
+    <div class="wrapper">
+    <h3 class="wrapper--title"><span>Rosturplast</span> продукты</h3>
+    <InfoCard 
+    :count="productsCount"
+    :total="totalResidualValue" />
+    <div class="wrapper--parent-card">
+      <div class="wrapper--parent-card--card" v-for="item in allDataRTP" :key="item.id">
+        <img :src="item.img" alt="Truba PPR" class="wrapper--parent-card--card--img" />
+        <div class="wrapper--parent-card--card--wrapper-title">
+          <p>{{ item.name }}</p>
+          <ul class="wrapper--parent-card--card--wrapper-title--property">
+            <li class="wrapper--parent-card--card--wrapper-title--property__item">
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">Бренд</div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__empty"></div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">{{ item.brand }}</div>
+            </li>
+            <li class="wrapper--parent-card--card--wrapper-title--property__item">
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">Количество</div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__empty"></div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">{{ item?.quantity}}</div>
+            </li>
+            <li class="wrapper--parent-card--card--wrapper-title--property__item">
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">Цена </div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__empty"></div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">{{ item?.price }} сом</div>
+            </li>
+            <li class="wrapper--parent-card--card--wrapper-title--property__item">
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">общий остаток</div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__empty"></div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">{{ item?.allResidual + " " + item?.unit }}</div>
+            </li>
+            <li class="wrapper--parent-card--card--wrapper-title--property__item" v-if="userRole !== 'agent'">
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">Оставшаяся сумма </div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__empty"></div>
+              <div class="wrapper--parent-card--card--wrapper-title--property__text">{{ item?.residualValue }} сом</div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
+
 </template>
 
 <script setup>
 import Navbar from "../../components/header/index.vue";
+import{ allDataRTP , productsCount , totalResidualValue} from "../../constants/rtp/index.js"
+import InfoCard from "../../components/card/infoCard/index.vue"
+
+const userRole = localStorage.getItem("userRole");
 
 const propsDate = {
   title: "Склад адир",
