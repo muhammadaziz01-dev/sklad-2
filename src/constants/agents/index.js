09362936fd75a67f62c5c15da0d0ce63d41097bd
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2026-09-02 16:48:30 +0600

    updet data

--- src/constants/agents/index.js
+++ src/constants/agents/index.js
@@ -181,9 +181,9 @@ export const agents = [
         phoneUrl : "",
         region : "Склад калелин калхоз",
         currency:"сом",
-        monthlySales: 3897345,
+        monthlySales: 3894045,
         monthlyCollected: 0,
-        totalDebt: 3897345,
+        totalDebt: 3894045,
         rol: "agent"
     },
 
